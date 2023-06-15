70eaad488c7e131348953bff611ffe2209be6511
gumelarme <gumelar.pn@gmail.com>
2023-06-15 13:50:34 +0700

    Configure nnn file manager

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -75,9 +75,24 @@
 
   programs.nnn = {
     enable = true;
+    package = pkgs.nnn.override({ withNerdIcons = true; });
     bookmarks = {};
     extraPackages = with pkgs; [ ffmpegthumbnailer mediainfo sxiv ];
-    plugins = {};
+    # TODO Add live previews and more plugins
+    plugins = {
+      mappings = {
+          c = "fzcd";
+          f = "finder";
+          v = "imgview";
+          z = "autojump";
+      };
+      src = (pkgs.fetchFromGitHub {
+          owner = "jarun";
+          repo = "nnn";
+          rev = "v4.8";
+          sha256 = "sha256-QbKW2wjhUNej3zoX18LdeUHqjNLYhEKyvPH2MXzp/iQ=";
+      }) + "/plugins";
+    };
   };
 
   programs.mcfly = {
