52ae7afa1eded37ca52f78258b2ab4c27fe59adf
gumelarme <gumelar.pn@gmail.com>
2023-06-23 16:35:46 +0700

    Change delta themes to dracula

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -43,6 +43,13 @@
     userEmail = "gumelar.pn@gmail.com";
     delta = {
       enable = true;
+      options = {
+        decorations = {
+          syntax-theme = "Dracula";
+        };
+        features = "decorations";
+        line-numbers = true;
+      };
     };
     aliases = {
       d = "diff";
