677f48feb03a57db62c058b0ea203122b2a690dc
gumelarme <gumelar.pn@gmail.com>
2023-07-06 20:46:34 +0700

    Move packages to default

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   imports = [
+    ./fonts.nix
+
     ./services
     ./xdg
 
@@ -12,7 +14,59 @@
     ./wezterm
     ./doom-emacs
 
-    ./fonts.nix
-    ./packages.nix
+  ];
+
+  home.packages = with pkgs; [
+    # Dev
+    xclip
+    docker
+    postman
+    python3Full
+    jetbrains.pycharm-community
+
+    # System utils
+    rsync
+    rclone
+    autorandr
+    rofi-power-menu
+    # gparted
+
+    # Documents
+    calibre
+    zathura
+    xournalpp
+    lorien
+    libreoffice-qt
+
+    # Media
+    vlc
+    tauon
+    spotify
+    nomacs
+    # blender
+    # godot
+    # inkscape
+    qbittorrent
+    nicotine-plus
+    # gimp  # annoyingly replaces xdg-mime for image/*
+
+    ## Kdenlive
+    # mediainfo
+    # glaxnimate
+    # libsForQt5.kdenlive
+
+    # Misc
+    steam
+    steam-run
+    discord
+    tdesktop
+    bitwarden
+    qutebrowser
+
+    # Peripheral
+    xsane
+    xboxdrv
+    libwacom
+    wacomtablet # KDE Config Module
   ];
 }
