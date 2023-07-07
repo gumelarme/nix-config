b81ef65db84d884bd1ceed4dd1b7e4c37f25b4cc
gumelarme <gumelar.pn@gmail.com>
2023-07-07 16:17:32 +0700

    Refactor: move firefox-css-hack as separate file

---
+++ home-manager/modules/firefox/css-hack.nix
@@ -0,0 +1,15 @@
+{pkgs, ... }:
+let
+  firefox-hack = pkgs.fetchFromGitHub {
+    owner = "MrOtherGuy";
+    repo = "firefox-csshacks";
+    rev = "2df24c36a2ca9d79783a4061ff6f98bb56131dc9";
+    hash = "sha256-QyB45dKbtLLs4FNeqNlL8RGq39mllWOLiCdqqqU5oxg=";
+  };
+  getFile = base: (file: builtins.readFile "${base}/${file}.css");
+  collectFiles = fileGetter: (filenames: builtins.concatStringsSep "\n" (map fileGetter filenames));
+in
+{
+  applyChromes = collectFiles (getFile (firefox-hack + "/chrome"));
+  applyContents = collectFiles (getFile (firefox-hack + "/content"));
+}
--- home-manager/modules/firefox/default.nix
+++ home-manager/modules/firefox/default.nix
@@ -10,22 +10,15 @@
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
       userChrome =
-        let
-          firefox-chrome-hack = (pkgs.fetchFromGitHub {
-                          owner = "MrOtherGuy";
-                          repo = "firefox-csshacks";
-                          rev = "2df24c36a2ca9d79783a4061ff6f98bb56131dc9";
-                          hash = "sha256-QyB45dKbtLLs4FNeqNlL8RGq39mllWOLiCdqqqU5oxg=";
-          }) + "/chrome";
-          getChromeFile = file: builtins.readFile "${firefox-chrome-hack}/${file}.css";
-          hacks = filenames: builtins.concatStringsSep "\n" (map getChromeFile filenames);
-        in
-          hacks [
-            "window_control_placeholder_support"
-            "hide_tabs_toolbar"
-            "compact_urlbar_megabar"
-            "privatemode_indicator_as_menu_button"
-          ];
+          let
+            firefox-hack = import ./css-hack.nix { inherit pkgs; };
+          in
+            firefox-hack.applyChromes [
+              "window_control_placeholder_support"
+              "hide_tabs_toolbar"
+              "compact_urlbar_megabar"
+              "privatemode_indicator_as_menu_button"
+            ];
       search = {
         force = true;
         default = "Google";
