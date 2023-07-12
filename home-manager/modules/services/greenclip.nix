159535a11d317d7142ff9952c6adde65829e98c3
gumelarme <gumelar.pn@gmail.com>
2023-07-12 16:47:23 +0700

    Add greenclip config file and fix service config

--- home-manager/modules/services/greenclip.nix
+++ home-manager/modules/services/greenclip.nix
@@ -1,16 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   home.packages = [ pkgs.haskellPackages.greenclip ];
+  home.file."${config.xdg.configHome}/greenclip.toml" = {
+    source = ./greenclip.toml;
+  };
+
   systemd.user.services.greenclip = {
     Unit = {
       Description = "greenclip daemon";
       After = [ "display-manager.service" ]; # X11
     };
     Service = {
-      Type = "forking";
-      ExecStart = "${pkgs.haskellPackages.greenclip}/bin/greenclip  daemon";
-      Restart = "always";
+      Type = "simple";
+      ExecStart = "${pkgs.haskellPackages.greenclip}/bin/greenclip daemon";
+      Restart = "on-failure";
     };
     Install = { WantedBy = [ "default.target" ]; };
   };
