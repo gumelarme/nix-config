223fbb0d68b5d3e047454709cb58b1d60f69ba78
gumelarme <gumelar.pn@gmail.com>
2024-01-27 01:18:28 +0800

    Convert git.nix to modules

--- home-manager/modules/shell/git.nix
+++ home-manager/modules/shell/git.nix
@@ -1,38 +1,62 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
+with lib;
+let cfg = config.modules.git;
 
-{
-  programs.git = {
-    enable = true;
-    userName = "gumelarme";
-    userEmail = "gumelar.pn@gmail.com";
-    delta = {
+in {
+  options.modules.git = {
+    global-gitignore = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      description = ''
+        List of globally ignored file,
+        useful if you dont want to pollute the .gitignore on project itself.
+      '';
+    };
+  };
+
+  config = {
+    programs.git = {
       enable = true;
-      options = {
-        decorations = { syntax-theme = "Dracula"; };
-        features = "decorations";
-        line-numbers = true;
+      userName = "gumelarme";
+      userEmail = "gumelar.pn@gmail.com";
+      extraConfig = {
+        core = {
+          excludesFile = let
+            ignored-str = concatStringsSep "\n" cfg.global-gitignore;
+            gitignore = (pkgs.writeText "global_gitignore" ignored-str);
+          in "${gitignore}";
+        };
+      };
+
+      delta = {
+        enable = true;
+        options = {
+          decorations = { syntax-theme = "Dracula"; };
+          features = "decorations";
+          line-numbers = true;
+        };
+      };
+
+      aliases = {
+        d = "diff";
+        ds = "diff --staged";
+        l = "log";
+        lo = "log --graph --oneline";
+        ls = "log --graph --name-status";
+        stashes = "stash list";
+        branches = "branch -a";
+        remotes = "remote -v";
+        uncommit = "reset --soft HEAD^";
+        graph =
+          "log --graph -10 --branches --remotes --tags --format=format:'%Cgreen%h %Creset• %<(75,trunc)%s (%cN, %cr) %Cred%d'";
       };
     };
-    aliases = {
-      d = "diff";
-      ds = "diff --staged";
-      l = "log";
-      lo = "log --graph --oneline";
-      ls = "log --graph --name-status";
-      stashes = "stash list";
-      branches = "branch -a";
-      remotes = "remote -v";
-      uncommit = "reset --soft HEAD^";
-      graph =
-        "log --graph -10 --branches --remotes --tags --format=format:'%Cgreen%h %Creset• %<(75,trunc)%s (%cN, %cr) %Cred%d'";
+
+    programs.gitui = {
+      enable = true;
+      keyConfig = builtins.readFile ./gitui/vim_style_key_config.ron;
     };
-  };
 
-  programs.gitui = {
-    enable = true;
-    keyConfig = builtins.readFile ./gitui/vim_style_key_config.ron;
+    xdg.configFile."gitui/key_symbols.ron".source = ./gitui/key_symbols.ron;
   };
-
-  xdg.configFile."gitui/key_symbols.ron".source = ./gitui/key_symbols.ron;
 }
-
