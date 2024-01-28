4515ea0316a62b451abc233c71f3a2d29391a6e7
gumelarme <gumelar.pn@gmail.com>
2024-01-28 20:16:52 +0800

    Convert eww into module

--- home-manager/modules/eww/default.nix
+++ home-manager/modules/eww/default.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
-
-{
-  home.packages = with pkgs; [ mpc-cli ffmpeg ];
-  programs.eww = {
-    enable = true;
-    configDir = ./.;
+{ pkgs, lib, config, ... }:
+with lib;
+let cfg = config.modules.eww;
+in {
+  options.modules.eww.enable = mkEnableOption "Enable eww configurations";
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ mpc-cli ffmpeg ];
+    programs.eww = {
+      enable = true;
+      configDir = ./.;
+    };
   };
 }
