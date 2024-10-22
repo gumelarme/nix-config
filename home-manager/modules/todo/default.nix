0e96965d5fe53c85444ff3aebe28f436e7803fc2
gumelarme <gumelar.pn@gmail.com>
2024-10-22 17:49:10 +0800

    feat(todo): default action `ls` + sort by priority

--- home-manager/modules/todo/default.nix
+++ home-manager/modules/todo/default.nix
@@ -15,6 +15,12 @@
         export DONE_FILE="$TODO_DIR/done.txt"
         export REPORT_FILE="$TODO_DIR/report.txt"
 
+        # todo.sh without argument
+        export TODOTXT_DEFAULT_ACTION="ls"
+
+        # Sort by priority
+        export TODOTXT_SORT_COMMAND='env LC_COLLATE=C sort -k 2,2 -k 1,1n'
+
         # You can customize your actions directory location
         # export TODO_ACTIONS_DIR="$HOME/.todo.actions.d"
       ''
