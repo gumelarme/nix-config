1922134fa639a7091828f63e37d4bef168d0b9a2
gumelarme <gumelar.pn@gmail.com>
2024-12-15 11:06:17 +0800

    hyprland: retain gapsin when grouped window exist

--- home-manager/modules/hyprland/decoration.nix
+++ home-manager/modules/hyprland/decoration.nix
@@ -52,7 +52,7 @@ _: {
   workspace = [
     # Replicate smart gaps
     "w[t1], gapsout:0, gapsin:0"
-    "w[tg1], gapsout:0, gapsin:0"
+    # "w[tg1], gapsout:0, gapsin:0"
     "f[1], gapsout:0, gapsin:0"
 
     # Make special workspace noticable
@@ -66,8 +66,8 @@ _: {
     # Replicate smart gaps
     "bordersize 0, floating:0, onworkspace:w[t1]"
     "rounding 0, floating:0, onworkspace:w[t1]"
-    "bordersize 0, floating:0, onworkspace:w[tg1]"
-    "rounding 0, floating:0, onworkspace:w[tg1]"
+    # "bordersize 0, floating:0, onworkspace:w[tg1]"
+    # "rounding 0, floating:0, onworkspace:w[tg1]"
     "bordersize 0, floating:0, onworkspace:f[1]"
     "rounding 0, floating:0, onworkspace:f[1]"
 
